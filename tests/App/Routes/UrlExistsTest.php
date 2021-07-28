143402c2141e2787e5e65be12ad7e6d2167b8a4c
fyzalis <julien.buabent@gmail.com>
2021-07-28 11:33:08 +0200

    renomme tests

--- tests/App/Routes/UrlExistsTest.php
+++ tests/App/Routes/UrlExistsTest.php
@@ -2,86 +2,45 @@
 
 namespace App\Routes;
 
-use CodeIgniter\Test\FeatureTestCase;
-
-
 use CodeIgniter\Test\DatabaseTestTrait;
+use CodeIgniter\Test\FeatureTestCase;
 use CodeIgniter\Test\FeatureTestTrait;
 
+class AddMemberTest extends FeatureTestCase {
 
-use App\Models\MemberModel;
-use CodeIgniter\Test\Fabricator;
-
-
-
-class UrlExistsTest extends FeatureTestCase
-{
- 
     use DatabaseTestTrait, FeatureTestTrait;
 
-
     //1 Créer les membre
-    public function testCreate(){
+    public function testAddMember() {
 
         /*$fabricator = new Fabricator(MemberModel::class, null, 'fr_FR');
         $testUser   = $fabricator->create(5);
         print_r($testUser);*/
 
-
         $result = $this->call('post', 'member', [
             'firstname' => 'Pierre',
-            'lastname' => 'Durand'
+            'lastname'  => 'Durand',
         ]);
 
         $this->assertTrue($result->isOK());
 
-
-
     }
 
-
-    public function testURLS(){
-        
-
-
-
-
-//2 Taper al routes members
-
-
-//3 Vérifier que les membres sont bien ceux créés
-
-
-
-        //$this->assertTrue(true);
+    public function testMemberCreated() {
 
         $result = $this->call('get', '/member');
 
-//$result = new \CodeIgniter\Test\TestResponse($response);
-
         $response = $result->response();
-
-        
-
-
 
         $result->assertJSONExact([
             [
-                "id" => "1", 
-                "firstname" => "Pierreg",
-                "lastname" => "Durand",
-                "instrument" => ""
-            ]
+                "id"         => "1",
+                "firstname"  => "Pierreg",
+                "lastname"   => "Durand",
+                "instrument" => "",
+            ],
         ]);
 
-
-        // foreach($this->urls as $url){
-        //     $result = $this->call('get', $url);        
-        //     $this->assertTrue($result->isOK());
-        // }
-        
-
     }
-
 
 }
