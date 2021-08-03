4627322725e034a62c6be1abcb835f80ccb31917
fyzalis <julien.buabent@gmail.com>
2021-08-03 11:04:48 +0200

    ajoute code méthode show

--- app/Controllers/Member.php
+++ app/Controllers/Member.php
@@ -1,122 +1,107 @@
 <?php
 
 namespace App\Controllers;
-use CodeIgniter\RESTful\ResourceController;
+use App\Models\MemberModel;
 use CodeIgniter\API\ResponseTrait;
 
-use App\Models\MemberModel;
+class Member extends BaseController {
 
+    use ResponseTrait;
 
-
-class Member extends BaseController
-{
-
-
-	use ResponseTrait;
-
-	protected $modelName = 'App\Models\MemberModel';
+    protected $modelName = 'App\Models\MemberModel';
     protected $format    = 'json'; //Permet de définir automatiquement le MIME type retourné
 
+    /**
+     * Present a view of resource objects
+     *
+     * @return mixed
+     */
+    public function index() {
+        $memberModel = new MemberModel();
+        $members     = $memberModel->findAll();
 
+        return $this->respond($members);
 
+    }
 
+    /**
+     * Present a view to present a specific resource object
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function show($id = null) {
+        $memberModel = new MemberModel();
+        $member      = $memberModel->find($id);
 
-	/**
-	 * Present a view of resource objects
-	 *
-	 * @return mixed
-	 */
-	public function index()
-	{
-		$memberModel = new MemberModel();
-		$members = $memberModel->findAll();
+        return $this->respond($member);
+    }
 
-		return $this->respond($members);
+    /**
+     * Present a view to present a new single resource object
+     *
+     * @return mixed
+     */
+    public function new () {
+        //
+    }
 
-	}
+    /**
+     * Process the creation/insertion of a new resource object.
+     * This should be a POST.
+     *
+     * @return mixed
+     */
+    public function create() {
+        $memberModel = new MemberModel();
+        $members     = $memberModel->insert($this->request->getPost());
 
-	/**
-	 * Present a view to present a specific resource object
-	 *
-	 * @param mixed $id
-	 *
-	 * @return mixed
-	 */
-	public function show($id = null)
-	{
-		//
-	}
+        return $this->respondCreated($members);
+    }
 
-	/**
-	 * Present a view to present a new single resource object
-	 *
-	 * @return mixed
-	 */
-	public function new()
-	{
-		//
-	}
+    /**
+     * Present a view to edit the properties of a specific resource object
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function edit($id = null) {
+        //
+    }
 
-	/**
-	 * Process the creation/insertion of a new resource object.
-	 * This should be a POST.
-	 *
-	 * @return mixed
-	 */
-	public function create()
-	{
-		$memberModel = new MemberModel();
-		$members = $memberModel->insert($this->request->getPost());
+    /**
+     * Process the updating, full or partial, of a specific resource object.
+     * This should be a POST.
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function update($id = null) {
+        //
+    }
 
-		return $this->respondCreated($members);
-	}
+    /**
+     * Present a view to confirm the deletion of a specific resource object
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function remove($id = null) {
+        //
+    }
 
-	/**
-	 * Present a view to edit the properties of a specific resource object
-	 *
-	 * @param mixed $id
-	 *
-	 * @return mixed
-	 */
-	public function edit($id = null)
-	{
-		//
-	}
-
-	/**
-	 * Process the updating, full or partial, of a specific resource object.
-	 * This should be a POST.
-	 *
-	 * @param mixed $id
-	 *
-	 * @return mixed
-	 */
-	public function update($id = null)
-	{
-		//
-	}
-
-	/**
-	 * Present a view to confirm the deletion of a specific resource object
-	 *
-	 * @param mixed $id
-	 *
-	 * @return mixed
-	 */
-	public function remove($id = null)
-	{
-		//
-	}
-
-	/**
-	 * Process the deletion of a specific resource object
-	 *
-	 * @param mixed $id
-	 *
-	 * @return mixed
-	 */
-	public function delete($id = null)
-	{
-		//
-	}
+    /**
+     * Process the deletion of a specific resource object
+     *
+     * @param mixed $id
+     *
+     * @return mixed
+     */
+    public function delete($id = null) {
+        //
+    }
 }
